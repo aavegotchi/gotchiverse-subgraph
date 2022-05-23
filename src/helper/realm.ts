a8107b879687aced1652089fb4a9376628b4fea2
Frank <frank.pfeift@protonmail.com>
2022-05-23 09:41:44 +0200

    added event handler and helper

--- src/helper/realm.ts
+++ src/helper/realm.ts
@@ -1,6 +1,6 @@
-import { BigInt } from "@graphprotocol/graph-ts";
-import { ChannelAlchemica } from "../../generated/RealmDiamond/RealmDiamond";
-import { ChannelAlchemicaEvent, Stat } from "../../generated/schema"
+import { BigInt, store } from "@graphprotocol/graph-ts";
+import { ChannelAlchemica, EquipInstallation, InstallationUpgraded, UnequipInstallation } from "../../generated/RealmDiamond/RealmDiamond";
+import { ChannelAlchemicaEvent, EquipInstallationEvent, InstallationUpgradedEvent, ParcelInstallation, Stat, UnequipInstallationEvent } from "../../generated/schema"
 import { BIGINT_ZERO, StatCategory, STAT_CATEGORIES } from "./constants";
 
 export const createChannelAlchemicaEvent = (event: ChannelAlchemica): ChannelAlchemicaEvent => {
@@ -19,6 +19,53 @@ export const createChannelAlchemicaEvent = (event: ChannelAlchemica): ChannelAlc
     return eventEntity;
 }
 
+export const createEquipInstallationEvent = (event: EquipInstallation): EquipInstallationEvent => {
+    let id = event.params._realmId.toString() + "-" + event.params._installationId.toString();
+    let eventEntity = new EquipInstallationEvent(id);
+    eventEntity.installationId = event.params._installationId;
+    eventEntity.realmId = event.params._realmId;
+    eventEntity.x = event.params._x;
+    eventEntity.y = event.params._y;
+    return eventEntity;
+}
+
+export const createUnequipInstallationEvent = (event: UnequipInstallation): UnequipInstallationEvent => {
+    let id = event.params._realmId.toString() + "-" + event.params._installationId.toString();
+    let eventEntity = new UnequipInstallationEvent(id);
+    eventEntity.installationId = event.params._installationId;
+    eventEntity.realmId = event.params._realmId;
+    eventEntity.x = event.params._x;
+    eventEntity.y = event.params._y;
+    return eventEntity;
+}
+
+export const createInstallationUpgradedEvent = (event: InstallationUpgraded): InstallationUpgradedEvent => {
+    let id = event.params._realmId.toString() + "-" + event.params._prevInstallationId.toString() + "-" + event.params._nextInstallationId.toString();
+    let eventEntity = new InstallationUpgradedEvent(id);
+    eventEntity.prevInstallationId = event.params._prevInstallationId;
+    eventEntity.nextInstallationId = event.params._nextInstallationId;
+    eventEntity.realmId = event.params._realmId;
+    eventEntity.x = event.params._coordinateX;
+    eventEntity.y = event.params._coordinateY;
+    return eventEntity;
+}
+
+export const createParcelInstallation = (realmId: BigInt, installationId: BigInt): ParcelInstallation  => {
+    let id = realmId.toString() + "-" + installationId.toString();
+    let installation = ParcelInstallation.load(id);
+    if(!installation) {
+        installation = new ParcelInstallation(id);
+        installation.realmId = realmId;
+        installation.installationId = installationId;
+    }
+    return installation;
+}
+
+export const removeParcelInstallation = (realmId: BigInt, installationId: BigInt): void => {
+    let id = realmId.toString() + "-" + installationId.toString();
+    store.remove("ParcelInstallation", id);
+}
+
 export const getStat = (category: StatCategory, entityId: BigInt): Stat => {
     let id = STAT_CATEGORIES[category];
     if(id != "overall") {
@@ -29,6 +76,7 @@ export const getStat = (category: StatCategory, entityId: BigInt): Stat => {
     if(!stats) {
         stats = new Stat(id);
         stats.countChannelAlchemicaEvents = BIGINT_ZERO;
+        stats.countParcelInstallations = BIGINT_ZERO;
     }
 
     return stats;
--- src/mappings/realm.ts
+++ src/mappings/realm.ts
@@ -1,6 +1,6 @@
-import { ChannelAlchemica } from "../../generated/RealmDiamond/RealmDiamond";
+import { ChannelAlchemica, EquipInstallation, InstallationUpgraded, UnequipInstallation } from "../../generated/RealmDiamond/RealmDiamond";
 import { BIGINT_ONE, BIGINT_ZERO, StatCategory } from "../helper/constants";
-import { createChannelAlchemicaEvent, getStat } from "../helper/realm";
+import { createChannelAlchemicaEvent, createEquipInstallationEvent, createInstallationUpgradedEvent, createParcelInstallation, createUnequipInstallationEvent, getStat, removeParcelInstallation } from "../helper/realm";
 
 export function handleChannelAlchemica(event: ChannelAlchemica): void  {
     // create and persist event
@@ -19,4 +19,46 @@ export function handleChannelAlchemica(event: ChannelAlchemica): void  {
     let overallStats = getStat(StatCategory.OVERALL, BIGINT_ZERO)
     overallStats.countChannelAlchemicaEvents = overallStats.countChannelAlchemicaEvents.plus(BIGINT_ONE);
     overallStats.save();
+}
+
+export function handleEquipInstallation(event: EquipInstallation): void {
+    let eventEntity = createEquipInstallationEvent(event);
+    eventEntity.save();
+
+    let installation = createParcelInstallation(event.params._realmId, event.params._installationId);
+    installation.save();
+
+    // update stats
+    let parcelStats = getStat(StatCategory.PARCEL, eventEntity.realmId)
+    parcelStats.countParcelInstallations = parcelStats.countParcelInstallations.plus(BIGINT_ONE);
+    parcelStats.save();
+
+    let overallStats = getStat(StatCategory.OVERALL, BIGINT_ZERO)
+    overallStats.countParcelInstallations = overallStats.countParcelInstallations.plus(BIGINT_ONE);
+    overallStats.save();
+}
+
+export function handleUnequipInstallation(event: UnequipInstallation): void {
+    let eventEntity = createUnequipInstallationEvent(event);
+    eventEntity.save();
+
+    removeParcelInstallation(event.params._realmId, event.params._installationId);
+
+    // update stats
+    let parcelStats = getStat(StatCategory.PARCEL, eventEntity.realmId)
+    parcelStats.countParcelInstallations = parcelStats.countParcelInstallations.minus(BIGINT_ONE);
+    parcelStats.save();
+
+    let overallStats = getStat(StatCategory.OVERALL, BIGINT_ZERO)
+    overallStats.countParcelInstallations = overallStats.countParcelInstallations.minus(BIGINT_ONE);
+    overallStats.save();
+}
+
+export function handleInstallationUpgraded(event: InstallationUpgraded): void {
+    let eventEntity = createInstallationUpgradedEvent(event);
+    eventEntity.save();
+
+    removeParcelInstallation(event.params._realmId, event.params._prevInstallationId);
+    let installation = createParcelInstallation(event.params._realmId, event.params._nextInstallationId);
+    installation.save();
 }
