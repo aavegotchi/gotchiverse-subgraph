169dc45f73baabbf08f4a65c40fc2f556bb54975
Frank <frank.pfeift@protonmail.com>
2023-02-09 09:34:19 +0100

    added tests

---
+++ tests/handleParcelAccessRightSet.test.ts
@@ -0,0 +1,61 @@
+import { BigInt, ethereum } from "@graphprotocol/graph-ts";
+import { assert, beforeAll, describe, newMockEvent, test } from "matchstick-as";
+import { ParcelAccessRightSet } from "../generated/RealmDiamond/RealmDiamond";
+import { BIGINT_ONE } from "../src/helper/constants";
+import { handleParcelAccessRightSet } from "../src/mappings/realm";
+
+let event: ParcelAccessRightSet;
+describe("handleParcelAccessRightSet", () => {
+    beforeAll(() => {
+        let mockEvent = newMockEvent();
+        event = new ParcelAccessRightSet(
+            mockEvent.address,
+            mockEvent.logIndex,
+            mockEvent.transactionLogIndex,
+            mockEvent.logType,
+            mockEvent.block,
+            mockEvent.transaction,
+            mockEvent.parameters,
+            null
+        );
+
+        event.parameters = new Array();
+
+        event.parameters.push(
+            new ethereum.EventParam(
+                "_realmId",
+                ethereum.Value.fromSignedBigInt(BIGINT_ONE)
+            )
+        );
+        event.parameters.push(
+            new ethereum.EventParam(
+                "_actionRight",
+                ethereum.Value.fromSignedBigInt(BIGINT_ONE)
+            )
+        );
+        event.parameters.push(
+            new ethereum.EventParam(
+                "_accessright",
+                ethereum.Value.fromSignedBigInt(BigInt.fromI32(2))
+            )
+        );
+        handleParcelAccessRightSet(event);
+    });
+
+    test("it should store an event entity with id of transaction hash and logIndex", () => {
+        let id =
+            event.transaction.hash.toHexString() +
+            "/" +
+            event.logIndex.toString();
+        assert.fieldEquals("ParcelAccessRightSetEvent", id, "realmId", "1");
+        assert.fieldEquals("ParcelAccessRightSetEvent", id, "actionRight", "1");
+        assert.fieldEquals("ParcelAccessRightSetEvent", id, "accessRight", "2");
+    });
+
+    test("it should create an entity with id of realm id and action right", () => {
+        let id = "1-1";
+        assert.fieldEquals("ParcelAccessRight", id, "parcel", "1");
+        assert.fieldEquals("ParcelAccessRight", id, "actionRight", "1");
+        assert.fieldEquals("ParcelAccessRight", id, "accessRight", "2");
+    });
+});
